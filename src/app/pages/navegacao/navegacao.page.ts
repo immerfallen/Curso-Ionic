1bffc7a27268aed93a00a67538a191174cd7067d
Maro de Melo <marodemelo10@gmail.com>
2021-10-11 17:04:23 -0300

    criando navegacao com botoes

--- src/app/pages/navegacao/navegacao.page.ts
+++ src/app/pages/navegacao/navegacao.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-navegacao',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavegacaoPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl:NavController) { 
+
+  }
 
   ngOnInit() {
+  }
+
+  showPageNavegacao2(){
+ this.navCtrl.navigateForward('navegacao2')
   }
 
 }
