75df4d5e84fa5b63790c09a85bbecb810942da3f
Maro de Melo <marodemelo10@gmail.com>
2021-10-12 11:44:29 -0300

    trabalhando com alert utilizando prompt

--- src/app/pages/alerta/alerta.page.ts
+++ src/app/pages/alerta/alerta.page.ts
@@ -8,22 +8,22 @@ import { AlertController } from '@ionic/angular';
 })
 export class AlertaPage implements OnInit {
 
-  constructor(private alertCtrl : AlertController ) { }
+  constructor(private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
-  async alert(){
+  async alert() {
     const alert = await this.alertCtrl.create({
       header: 'Alert',
       subHeader: 'Subtitle',
       message: 'This is an alert message',
       buttons: ['OK']
     })
-   await alert.present()
+    await alert.present()
   }
 
-  async multiple(){
+  async multiple() {
     const alert = await this.alertCtrl.create({
       header: 'Alert',
       subHeader: 'Subtitle',
@@ -33,23 +33,58 @@ export class AlertaPage implements OnInit {
     await alert.present()
   }
 
-  async confirm(){
+  async confirm() {
     const alert = await this.alertCtrl.create({
-      header: 'Confirm!',     
+      header: 'Confirm!',
       message: 'This <strong>text</strong>!!!',
       buttons: [{
         text: 'Cancel',
         role: 'cancel',
         cssClass: 'secondary',
-        handler: (blah)=> {
+        handler: (blah) => {
           console.log('Confirm cancel: blah')
         }
       }, {
         text: 'okay',
-        handler: ()=> {
+        handler: () => {
           console.log('Confirm okay')
         }
       }]
+    })
+    await alert.present()
+  }
+
+  async prompt() {
+    const alert = await this.alertCtrl.create({
+      header: 'Acesso restrito!',
+      inputs: [{
+        name: 'email',
+        type: 'text',
+        placeholder: 'Informe seu E-mail'
+      },
+      {
+        name: 'senha',
+        type: 'password',
+        id: 'password',
+        value: 'hello',
+        placeholder: 'Informe sua senha',
+      },      
+    ],
+    buttons: [
+      {        
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm cancel: blah')
+          }
+        }, {
+          text: 'okay',
+          handler: (form) => {
+            console.log(form)
+          }
+        }      
+    ]
     })
     await alert.present()
   }
