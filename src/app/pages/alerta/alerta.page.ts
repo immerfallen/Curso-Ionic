513eba4cca5f932b5e24b34fe2843fbe6da61a75
Maro de Melo <marodemelo10@gmail.com>
2021-10-12 10:22:56 -0300

    estudando alert

--- src/app/pages/alerta/alerta.page.ts
+++ src/app/pages/alerta/alerta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alerta',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertaPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertCtrl : AlertController ) { }
 
   ngOnInit() {
   }
 
+  async alert(){
+    const alert = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message',
+      buttons: ['OK']
+    })
+   await alert.present()
+  }
+
+  async multiple(){
+    const alert = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message',
+      buttons: ['Cancel', 'Open Modal', 'Delete']
+    })
+    await alert.present()
+  }
+
+  async confirm(){
+    const alert = await this.alertCtrl.create({
+      header: 'Confirm!',     
+      message: 'This <strong>text</strong>!!!',
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: (blah)=> {
+          console.log('Confirm cancel: blah')
+        }
+      }, {
+        text: 'okay',
+        handler: ()=> {
+          console.log('Confirm okay')
+        }
+      }]
+    })
+    await alert.present()
+  }
 }
