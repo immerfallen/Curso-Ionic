e772e494ea2ed85e954bbb088984403016193752
Maro de Melo <marodemelo10@gmail.com>
2021-10-13 16:48:03 -0300

    estudando o componente segment

--- src/app/pages/segment/segment.page.ts
+++ src/app/pages/segment/segment.page.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./segment.page.scss'],
 })
 export class SegmentPage implements OnInit {
-
+  opcaoSelecionada: string = 'frutas'
   constructor() { }
 
   ngOnInit() {
   }
+selecionar(e: any){
+console.log(e)
 
+this.opcaoSelecionada = e.detail.value
+}
 }
