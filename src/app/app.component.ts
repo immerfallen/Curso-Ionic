7bf25aa21873f753444a6da40e8968f390663e6a
Maro de Melo <marodemelo10@gmail.com>
2021-10-13 09:02:53 -0300

    estudando o componente datetime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
     { title: 'Checkbox', url: 'checkbox', icon: 'construct' },
     { title: 'Chip', url: 'chip', icon: 'construct' },
     { title: 'Content', url: 'content', icon: 'construct' },
+    { title: 'DateTime', url: 'datetime', icon: 'construct' },
 
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
