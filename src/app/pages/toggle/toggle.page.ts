e3bc52399f26edb7ed31b18056bc15f6a676ecef
Maro de Melo <marodemelo10@gmail.com>
2021-10-13 15:18:46 -0300

    estudando toggle

--- src/app/pages/toggle/toggle.page.ts
+++ src/app/pages/toggle/toggle.page.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toggle.page.scss'],
 })
 export class TogglePage implements OnInit {
+  marcado: boolean = true
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+ngOnInit() {
+}
 
 }
