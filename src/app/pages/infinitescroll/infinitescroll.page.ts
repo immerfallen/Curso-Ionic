16366db90d03fb798b09bb749ce5205cfe6d4815
Maro de Melo <marodemelo10@gmail.com>
2021-10-13 10:43:17 -0300

    criando pagina para estudar infinite scroll

---
+++ src/app/pages/infinitescroll/infinitescroll.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-infinitescroll',
+  templateUrl: './infinitescroll.page.html',
+  styleUrls: ['./infinitescroll.page.scss'],
+})
+export class InfinitescrollPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
