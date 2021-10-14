be846c79b3fc152d1cb9714e21ccca43e2367435
Maro de Melo <marodemelo10@gmail.com>
2021-10-14 08:19:04 -0300

    estudando componente toast

--- src/app/pages/toast/toast.page.ts
+++ src/app/pages/toast/toast.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-toast',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToastPage implements OnInit {
 
-  constructor() { }
+  constructor(private toastCtrl: ToastController) { }
 
   ngOnInit() {
   }
+  async toastSimples() {
+    let toast = await this.toastCtrl.create({
+      message: 'Ola mundo',
+      duration: 2000
+    })
+    toast.present()
+  }
 
+  async toastComOpcoes() {
+    const toast = await this.toastCtrl.create({
+      header : 'Toast header',
+      message : 'Click to Close',
+      position : 'top',
+      buttons : [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    })
+    toast.present()
+  }
 }
